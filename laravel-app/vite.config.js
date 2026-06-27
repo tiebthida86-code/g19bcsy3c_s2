f94ee1e3c467d08dda0a1ed55887a4c100f4222f
tiebthida86-code <tiebthida86@gmail>
2026-06-27 15:50:12 +0700

    vue-router session 3.0

--- laravel-app/vite.config.js
+++ laravel-app/vite.config.js
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        tailwindcss(),
+        vue(),
+        vueDevTools(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
-});
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            protocol: 'ws',
+            port: 5173,
+        },
+        watch: {
+            usePolling: true,
+            useFsEvents: true,
+            interval: 1000,
+        }
+    }
+})
