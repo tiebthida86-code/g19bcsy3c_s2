ad8b04d6313f1706bfcbe9930d5a86cd19893b82
tiebthida86-code <tiebthida86@gmail>
2026-07-13 22:25:59 +0700

    create sendverify and EmailVerify session 3.3

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/signin', [AuthController::class, 'signin']);
+Route::get('/verify/email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+    ->middleware('signed')
+    ->name('verify.email');
+Route::post('/send/verification-email', [AuthController::class, 'sendVerificationEmail']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/signout', [AuthController::class, 'signout']);
